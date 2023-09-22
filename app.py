cdfa196743f9196e786b2a87f415ab84293a591b
Shreypay <39966989+Shreypay@users.noreply.github.com>
2023-09-21 19:47:23 -0700

    temp commented sklearn import

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import math
 import streamlit as st
 import numpy as np
 import pandas as pd
-from sklearn.preprocessing import MinMaxScaler
+# from sklearn.preprocessing import MinMaxScaler
 from keras.models import Sequential
 from keras.layers import Dense, LSTM
 import matplotlib.pyplot as plt
